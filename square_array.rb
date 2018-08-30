d47fdc6250024820e9bd4088de60e38e30f15041
Ashley Bland <ashleyjbland@gmail.com>
2018-08-30 22:42:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1, 2, 3]
 
 def square_array(array)
   square_numbers = []
-  square = array.each do |number|
+  array.each do |number|
     puts number**2
     square_numbers << number**2
   end
